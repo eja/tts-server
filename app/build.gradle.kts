1c60c22462f4da5a233770249a941d02111125c0
Ubaldo Porcheddu <ubaldo@eja.it>
2024-05-28 15:23:25 +0100

    increase version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "it.eja.ttsserver"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.5.28"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
